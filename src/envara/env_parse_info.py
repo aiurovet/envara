8b338027e9474c29c5fb0eb880c3291a7e045f3f
Alexander Iurovetski <aiurovet@gmail.com>
2026-01-21 01:23:05 +1100

    WIP

--- src/envara/env_parse_info.py
+++ src/envara/env_parse_info.py
@@ -11,6 +11,91 @@ from env_quote_type import EnvQuoteType
 
 ###############################################################################
 
+#import re
+def test():
+    input = "\\\\$HOME"
+    esc = r'\\'
+
+    regex = re.compile(
+        f"(^({esc}{esc})*|[^{esc}]({esc}{esc})*)\\$({{?)([A-Za-z_][A-Za-z_\\d]*)(}}?)",
+        re.UNICODE
+    )
+
+    def parser(m: re.Match) -> str:
+        g = m.groups()
+
+        g1 = "None" if g[0] is None else f"\"{g[0]}\""
+        g2 = "None" if g[1] is None else f"\"{g[1]}\""
+        g3 = "None" if g[2] is None else f"\"{g[2]}\""
+        g4 = "None" if g[3] is None else f"\"{g[3]}\""
+        g5 = "None" if g[4] is None else f"\"{g[4]}\""
+        g6 = "None" if g[5] is None else f"\"{g[5]}\""
+
+        #print(f"1: {g1}, 2: {g2}, 3: {g3}, 4: {g4}, 5: {g5}, 6: {g6}")
+        print(f"1: {g1}, 2: {g2}, 3: {g3}, 4: {g4}, 5: {g5}, 6: {g6}")
+
+        return ""
+
+    _ = regex.sub(parser, input)
+
+
+###############################################################################
+
+def get_escape_solver(
+    escape: str,
+) -> str:
+    """
+    Create a string that allows to skip the even number of escapes and
+    ignore the following pattern if there is one more escape after
+    
+    :param escape: Plain escape character
+    :type escape: str
+    :return: Composed string to become a prefix in a pattern
+    :rtype: str
+    """
+    esc = re.escape(escape)
+    return f"(^({esc}{esc})*|[^{esc}]({esc}{esc})*)"
+
+
+###############################################################################
+
+def create_env_pattern(
+    escape: str,
+    expand: str,
+    braces: str,
+    fodder: str,
+    is_symmetric: bool = False
+) -> re.Pattern:
+    """
+    Create a string that allows to skip the even number of escapes and
+    ignore the following pattern if there is one more escape after
+    
+    :param escape: Plain escape character
+    :type escape: str
+    :param expand: Plain expand character
+    :type expand: str
+    :param braces: Two enclosing braces or empty string
+    :type braces: str
+    :param fodder: The actual pattern string beyond the expand character
+    :type fodder: str
+    :param is_symmetric: True = surround fodder with expanders from both sides
+    :type is_symmetric: bool
+    :return: Compiled regular expression
+    :rtype: re.Pattern
+    """
+
+    esc = re.escape(escape)
+    exp = re.escape(expand)
+
+    exp_grp = f"({exp})"
+
+    pat: str = f"(^({esc}{esc})*|[^{esc}]({esc}{esc})*)" + \
+        f"{exp_grp}{fodder}{exp_grp}" if is_symmetric else f"{exp}{fodder}"
+    
+    return re.compile(pat, EnvParseInfo.RE_FLAGS)
+
+###############################################################################
+
 
 class EnvParseInfo:
     """
@@ -20,32 +105,9 @@ class EnvParseInfo:
 
     # Pre-defined constants
 
-import re
+    NO_BRACES: ClassVar[str] = ""
 
-input = "`$HOME"
-
-#(^|[^`])(``)*
-regex = re.compile(
-    "\\$({?)([A-Za-z_][A-Za-z_\\d]*)(}?)",
-    re.IGNORECASE | re.UNICODE
-)
-
-def parser(m: re.Match) -> str:
-    g = m.groups()
-
-    g1 = "None" if g[0] is None else f"\"{g[0]}\""
-    g2 = "None" if g[1] is None else f"\"{g[1]}\""
-    #g3 = "None" if g[2] is None else f"\"{g[2]}\""
-    #g4 = "None" if g[3] is None else f"\"{g[3]}\""
-    #g5 = "None" if g[4] is None else f"\"{g[4]}\""
-
-    #print(f"1: {g1}, 2: {g2}, 3: {g3}, 4: {g4}, 5: {g5}")
-    print(f"1: {g1}, 2: {g2}")
-
-    return ""
-
-    _ = regex.sub(parser, input)
-
+    POSIX_BRACES: ClassVar[str] = "{}"
     POSIX_ESCAPE: ClassVar[str] = "\\"
     POSIX_EXPAND: ClassVar[str] = "$"
 
@@ -55,40 +117,45 @@ def parser(m: re.Match) -> str:
     WINDOWS_ESCAPE: ClassVar[str] = "^"
     WINDOWS_EXPAND: ClassVar[str] = "%"
 
-    RE_FLAGS: ClassVar[int] = re.IGNORECASE | re.UNICODE
+    RE_FLAGS: ClassVar[int] = re.UNICODE
+    RE_AL_NUM: ClassVar[str] = r"([A-Za-z_][A-Za-z_\d]*)"
+    RE_MINIMAL: ClassVar[str] = r"([^=:]+)"
+    RE_NUMBER: ClassVar[str] = r"([\d]+)"
 
     # Helps to find env var parttern based on expand and escape
 
     ARG_PATTERNS: ClassVar[dict[str, re.Pattern]] = {
-        POSIX_EXPAND + POSIX_ESCAPE:
-            re.compile("(^|[^\\\\])(\\\\)*\\$({?)(\\d+)(}?)", RE_FLAGS
+        POSIX_EXPAND + POSIX_ESCAPE: create_env_pattern(
+            POSIX_ESCAPE, POSIX_EXPAND, POSIX_BRACES, RE_NUMBER
         ),
-        PWSH_EXPAND + PWSH_ESCAPE:
-            re.compile("(^|[^`])(``)*\\({?)(\\d+)(}?)", RE_FLAGS
+        PWSH_EXPAND + PWSH_ESCAPE: create_env_pattern(
+            PWSH_ESCAPE, PWSH_EXPAND, POSIX_BRACES, RE_NUMBER
         ),
-        WINDOWS_EXPAND + POSIX_ESCAPE:
-            re.compile("(^|[^\\\\])(\\\\)*(%)(\\d+)", RE_FLAGS
+        WINDOWS_EXPAND + POSIX_ESCAPE: create_env_pattern(
+            POSIX_ESCAPE, WINDOWS_EXPAND, NO_BRACES, RE_NUMBER
         ),
-        WINDOWS_EXPAND + WINDOWS_ESCAPE:
-            re.compile("(^|[^\\^\\^])(\\^\\^)*(%)(\\d+)(%)", RE_FLAGS
-        ),
+        WINDOWS_EXPAND + WINDOWS_ESCAPE: create_env_pattern(
+            WINDOWS_ESCAPE, WINDOWS_EXPAND, NO_BRACES, RE_NUMBER
+        )
     }
 
     # Helps to find env var parttern based on expand and escape
 
     VAR_PATTERNS: ClassVar[dict[str, re.Pattern]] = {
-        POSIX_EXPAND + POSIX_ESCAPE:
-            re.compile("(^|[^\\\\])(\\\\)*\\$({?)([A-Za-z_][A-Za-z_\\d]*)(}?)", RE_FLAGS
+        POSIX_EXPAND + POSIX_ESCAPE: create_env_pattern(
+            POSIX_ESCAPE, POSIX_EXPAND, POSIX_BRACES, RE_AL_NUM
         ),
-        PWSH_EXPAND + PWSH_ESCAPE:
-            re.compile("([``]*)\\$({?)([A-Za-z_][A-Za-z_\\d]*)(}?)", RE_FLAGS
+        PWSH_EXPAND + PWSH_ESCAPE: create_env_pattern(
+            PWSH_ESCAPE, PWSH_EXPAND, POSIX_BRACES, RE_AL_NUM
         ),
-        WINDOWS_EXPAND + POSIX_ESCAPE:
-            re.compile("([\\\\]*)(%)([A-Za-z_][A-Za-z_\\d]*)(%)", RE_FLAGS
+        WINDOWS_EXPAND + POSIX_ESCAPE: create_env_pattern(
+            POSIX_ESCAPE, WINDOWS_EXPAND, NO_BRACES, RE_MINIMAL,
+            is_symmetric=True
         ),
-        WINDOWS_EXPAND + WINDOWS_ESCAPE:
-            re.compile("([\\^]*)(%)([A-Za-z_][A-Za-z_\\d]*)(%)", RE_FLAGS
-        ),
+        WINDOWS_EXPAND + WINDOWS_ESCAPE: create_env_pattern(
+            WINDOWS_ESCAPE, WINDOWS_EXPAND, NO_BRACES, RE_MINIMAL,
+            is_symmetric=True
+        )
     }
 
     ###########################################################################
@@ -137,8 +204,8 @@ def parser(m: re.Match) -> str:
 
     @staticmethod
     def find_patterns(
-        expand: str = None,
         escape: str = None,
+        expand: str = None,
     ) -> tuple[re.Pattern, re.Pattern]:
         """
         Find pattern for environment variables' and arguments' placeholders
@@ -165,11 +232,12 @@ def parser(m: re.Match) -> str:
         # If expand and escape form a valid key to the set of patterns,
         # return the found pattern. Otherwise, None
 
-        key: str = f"{expand}{escape}"
+        key: str = f"{escape}{expand}"
 
         return (
             EnvParseInfo.ARG_PATTERNS.get(key),
             EnvParseInfo.VAR_PATTERNS.get(key)
         )
+
 
 ###############################################################################
